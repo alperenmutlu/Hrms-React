b133464013222ab59c034213c9a4be6fac582d00
Alperen Mutlu <chetto_17@hotmail.com>
2021-06-23 17:19:23 +0300

    Düzenlendi.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
-​import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css/semantic.min.css'
+import Navi from './layouts/Navi';
+import { Container } from 'semantic-ui-react';
+import Dashboard from './layouts/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navi/>
+      <Container className="main">
+        <Dashboard/>
+      </Container>
     </div>
   );
 }
